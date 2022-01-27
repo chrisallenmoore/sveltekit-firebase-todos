5beaa45256af4c7056fddc4c4dbc34979b24682b
Chris Allen Moore <chris@chrisallenmoore.com>
2022-01-27 18:27:30 +0700

    pull data from Firebase

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBbZGarJcE1C0wuC2gF_X89l_HbZVIN4fg",
+    authDomain: "sveltekit-todos-8f216.firebaseapp.com",
+    projectId: "sveltekit-todos-8f216",
+    storageBucket: "sveltekit-todos-8f216.appspot.com",
+    messagingSenderId: "144199186942",
+    appId: "1:144199186942:web:df0dd443d47bb879ea68af"
+};
